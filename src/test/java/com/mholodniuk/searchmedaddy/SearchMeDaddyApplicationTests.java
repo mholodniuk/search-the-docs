cc5ed53bdc973fec856daeb60563c691aec064de
maciejholodniuk <maciekholo@gmail.com>
2023-07-12 22:09:53 +0200

    init

---
+++ src/test/java/com/mholodniuk/searchmedaddy/SearchMeDaddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mholodniuk.searchmedaddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchMeDaddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
